2a9a9812ea00a1b85794a5c498d2a91ab400008b
ronel0822 <44457486+ronel0822@users.noreply.github.com>
2021-01-02 10:52:27 +0800

    Revert "Revert "Upload file""
    This reverts commit dd3a95748703df3a5deb56c23657437c7281b8f6.

---
+++ main.php
@@ -0,0 +1,24 @@
+
+<?php 
+  include 'header.php';
+  include 'navigation.php';
+?>
+<!-- Page Heading -->
+<div class="col-sm-12 align-items-center justify-content-between mb-4">
+  <center><h1 class="h3 mb-0 text-gray-800">Homis Analytics</h1></center>
+</div>
+
+<div class="container">
+  <canvas id="myChart"></canvas>
+</div>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Content Row -->
+          <div class="row"></div>
+      <!-- End of Main Content -->
+<?php  
+  include 'footer.php';
+?>
